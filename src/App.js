e58904fed4cd2361ddf177bde31d3e60789b125c
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-12 23:00:40 -0400

    first steps

--- src/App.js
+++ src/App.js
@@ -3,22 +3,49 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>
+        $400<span>.00</span></h1>
+        <form>
+        <div className="basic">
+          <input type="text" placeholder={'+200 new samsung tv'}/>
+          <input type="datetime-local"/>
+        </div>
+          <div className="description">
+            <input type="text" placeholder={'description'}/>
+          </div>
+          <button type="submit">Add New Transaction</button>
+        </form>
+        <div className="transactions">
+          <div className="transaction">
+            <div className="left">
+              <div className="name">New samsung TV</div>
+              <div className="description">Impulsive buyer</div>
+            </div>
+            <div className="right"></div>
+              <div className="price">$500</div>
+              <div className="datetime">2023-04-12 13:30</div>
+          </div>
+          <div className="transaction">
+            <div className="left">
+              <div className="name">New samsung TV</div>
+              <div className="description">Impulsive buyer</div>
+            </div>
+            <div className="right"></div>
+              <div className="price">$500</div>
+              <div className="datetime">2023-04-12 13:30</div>
+          </div>
+          <div className="transaction">
+            <div className="left">
+              <div className="name">New samsung TV</div>
+              <div className="description">Impulsive buyer</div>
+            </div>
+            <div className="right"></div>
+              <div className="price">$500</div>
+              <div className="datetime">2023-04-12 13:30</div>
+          </div>
+        </div>
+    </main>
   );
 }
 
