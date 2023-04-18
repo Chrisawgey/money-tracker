d1566fd9b9deeb7cd3b44811d261108b469a870d
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-18 01:49:27 +0000

    In the works.. mongo compiling

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const Transaction = require('./models/Transaction.js');
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(cors());
@@ -9,8 +12,11 @@ app.get('/api/test', (req, res) => {
   });
 
   
-  app.post("/api/transaction", (req, res) =>{
-    res.json(req.body);
+  app.post("/api/transaction", async (req, res) =>{
+    await mongoose.connect(process.env.MONGO_URL)
+    const {name,description,datetime} = req.body;
+    const transaction = await Transaction.create({name,description,datetime});
+    res.json(transaction);
   });
 
   app.listen(4040,)
